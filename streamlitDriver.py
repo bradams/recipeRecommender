e5b41a40ebc412f44eda0b63c84c0273e16ae267
bradams <bradleyadams2017@gmail.com>
2022-12-29 17:31:41 -0500

    Changes

--- streamlitDriver.py
+++ streamlitDriver.py
@@ -1,4 +1,11 @@
 import streamlit as st
+from PIL import Image
+import os
+
+image1 = Image.open('C:/Users/bradl/OneDrive/Desktop/Git/recipeRecommender/testPicture.jpg')
+st.image(image1)
+
+
 
 st.title("This is the title")
 
